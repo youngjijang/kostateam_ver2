1d976e58b9a1efb4f796766e0ba6ff4e2e1a2270
rmflsms <84006878+rmflsms@users.noreply.github.com>
2022-03-08 18:20:09 +0900

    커밋

--- src/academy/mvc/controller/BoardController.java
+++ src/academy/mvc/controller/BoardController.java
@@ -1,6 +1,52 @@
 package academy.mvc.controller;
 
-public class BoardController {
+import java.sql.SQLException;
+import java.util.List;
 
+import academy.mvc.model.dto.BoardDTO;
+import academy.mvc.model.dto.ReplyDTO;
+import academy.mvc.service.BoardService;
+import academy.mvc.service.BoardServiceImpl;
+import academy.mvc.view.FailView;
+import academy.mvc.view.SuccessView;
+
+
+
+public class BoardController {
+	  private static BoardService boardService = new BoardServiceImpl();
+	  	 
+	  /**
+	    * 전체검색
+	    * */
+	   public static void boardSelectByAll() {
+		   try {
+		    List<BoardDTO> list = boardService.boardSelectAll();
+		     SuccessView.printBoardList(list);
+		   }catch (SQLException e) {
+			  //e.printStackTrace();
+			  FailView.errorMessage(e.getMessage()); 
+		   }
+	   }
+
+		/**
+		 * 댓글등록하기
+		 * */
+		public static void replyInsert(ReplyDTO replyDTO) {
+			try {
+		       boardService.replyInsert(replyDTO);  
+		       SuccessView.printMessage(replyDTO.getBoardNo()+" 글에 댓글이 등록되었습니다.");
+			}catch (SQLException e) {
+				//e.printStackTrace();
+				FailView.errorMessage("댓글등록에 실패하였습니다.");
+				
+			}catch (RuntimeException e) {
+				FailView.errorMessage(e.getMessage());
+			}
+		}
+
+		public static void main(String[] args) {
+	System.out.println();
 }
-//ㅎ
+		
+}
+//
