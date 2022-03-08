7f2b791e436f5bf12abb5dee03393182172ec135
youngji <younji.jang804@gmail.com>
2022-03-08 17:02:18 +0900

    reply DTO수정

--- src/academy/mvc/model/dto/ReplyDTO.java
+++ src/academy/mvc/model/dto/ReplyDTO.java
@@ -5,68 +5,81 @@ public class ReplyDTO {
 	private int replyNo; //댓글번호
 	private String replyContent; //댓글내용
 	private int boardNo; //글번호 
+	private String replyName;//글쓴이
+	private int replyPwd;//비번
 	private String replyRegdate;
 
 	  
-	   public ReplyDTO() {}
-	   
-	   public ReplyDTO( String replyContent, int boardNo) {
-			this.replyContent = replyContent;
-			this.boardNo = boardNo;
-	   }
-
-	   public ReplyDTO(int replyNo, String replyContent, int boardNo, String replyRegdate) {
-		this(replyContent, boardNo);
-		
+	public ReplyDTO() {}
+	
+	
+	public ReplyDTO(int replyNo, String replyContent, int boardNo, String replyName, int replyPwd) {
+		super();
 		this.replyNo = replyNo;
+		this.replyContent = replyContent;
+		this.boardNo = boardNo;
+		this.replyName = replyName;
+		this.replyPwd = replyPwd;
+	}
+
+
+	public int getReplyNo() {
+		return replyNo;
+	}
+
+
+	public void setReplyNo(int replyNo) {
+		this.replyNo = replyNo;
+	}
+
+
+	public String getReplyContent() {
+		return replyContent;
+	}
+
+
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+
+
+	public int getBoardNo() {
+		return boardNo;
+	}
+
+
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+
+
+	public String getReplyName() {
+		return replyName;
+	}
+
+
+	public void setReplyName(String replyName) {
+		this.replyName = replyName;
+	}
+
+
+	public int getReplyPwd() {
+		return replyPwd;
+	}
+
+
+	public void setReplyPwd(int replyPwd) {
+		this.replyPwd = replyPwd;
+	}
+
+
+	public String getReplyRegdate() {
+		return replyRegdate;
+	}
+
+
+	public void setReplyRegdate(String replyRegdate) {
 		this.replyRegdate = replyRegdate;
-	  }
-
-		public int getReplyNo() {
-			return replyNo;
-		}
-		
-		public void setReplyNo(int replyNo) {
-			this.replyNo = replyNo;
-		}
-		
-		public String getReplyContent() {
-			return replyContent;
-		}
-		
-		public void setReplyContent(String replyContent) {
-			this.replyContent = replyContent;
-		}
-		
-		public int getBoardNo() {
-			return boardNo;
-		}
-		
-		public void setBoardNo(int boardNo) {
-			this.boardNo = boardNo;
-		}
-		
-		public String getReplyRegdate() {
-			return replyRegdate;
-		}
-		
-		public void setReplyRegdate(String replyRegdate) {
-			this.replyRegdate = replyRegdate;
-		}
-
-		
-		@Override
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			builder.append("ReplyDTO [replyNo=");
-			builder.append(replyNo);
-			builder.append(", replyContent=");
-			builder.append(replyContent);
-			builder.append(", boardNo=");
-			builder.append(boardNo);
-			builder.append(", replyRegdate=");
-			builder.append(replyRegdate);
-			builder.append("]");
-			return builder.toString();
-		}
+	}
+	  
 }
