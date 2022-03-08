2d7e44d6febaa33c1932ccb6669e07db5bde4f98
youngji <younji.jang804@gmail.com>
2022-03-08 11:57:25 +0900

    second commit -encoding test

--- src/academy/mvc/session/Session.java
+++ src/academy/mvc/session/Session.java
@@ -1,5 +1,7 @@
 package academy.mvc.session;
-
+/**
+ * 각회원의 관련된 정보 저장하는 객
+ * */
 public class Session {
 
 	//
--- src/academy/mvc/session/SessionSet.java
+++ src/academy/mvc/session/SessionSet.java
@@ -1,5 +1,7 @@
 package academy.mvc.session;
-
+/**
+ * 모든 USER 즉 Session을 관리하는 객체 
+*/
 public class SessionSet {
 
 }//ㅎ
