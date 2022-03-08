49681558e60d70ba5fcf5ff43910f9d0aeb19ff9
jeongah <99481661+pivotCosmos@users.noreply.github.com>
2022-03-08 14:52:59 +0900

    update SessionSet

--- src/academy/mvc/session/SessionSet.java
+++ src/academy/mvc/session/SessionSet.java
@@ -2,6 +2,52 @@ package academy.mvc.session;
 /**
  * 모든 USER 즉 Session을 관리하는 객체 
 */
-public class SessionSet {
+import java.util.HashSet;
+import java.util.Set;
+
+public class SessionSet {//싱글톤
+
+	private static SessionSet ss = new SessionSet();
+	private Set<Session> set;
+	
+	private SessionSet() {
+		set = new HashSet<>();
+	}
+	
+	public static SessionSet getInstance() {
+		return ss;
+	}
+	
+	
+	/**
+	 * 사용자 찾기
+	 * */
+	public Session get(String sessionId) {
+		for(Session session : set) {
+			if(session.getSessionId().equals(sessionId)) {
+				return session;
+			}
+		}
+		return null;
+	}
+	
+	//세션 객체들 반환
+		public Set<Session> getSet(){
+			return set;
+		}
+	
+		/**
+		 * 로그인 된 사용자 추가
+		 * */
+		public void add(Session session) {
+			set.add(session);
+		}
+		
+	/**
+	 * 사용자 제거 - 로그아웃
+	 * */
+	public void remove(Session session) {
+		set.remove(session);
+	}
 
 }
