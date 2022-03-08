39f3b210072701659bf67c5c5f849acf80da0aeb
jeongah <99481661+pivotCosmos@users.noreply.github.com>
2022-03-08 17:09:34 +0900

    delete userLogin from StartView, add userLoginMenu to MenuView

--- src/academy/mvc/view/StartView.java
+++ src/academy/mvc/view/StartView.java
@@ -1,43 +1,9 @@
 package academy.mvc.view;
 
-import java.util.Scanner;
-
-import academy.mvc.controller.UserController;
-
 public class StartView {
 
-	static Scanner sc = new Scanner(System.in);
-	public static void userLogin() {
-		
-		String userId;
-		int userPwd;
-		String kind;
-		
-		while(true){
-			System.out.println("-------------Academy Program 시작합니다-------------");
-			System.out.println("사용자 유형을 입력하세요");
-			System.out.print("학생계정-1  강사계정-2 > ");
-			kind = sc.nextLine();
-			
-			System.out.print("아이디> ");
-			userId = sc.nextLine();
-			System.out.print("비밀번호> ");
-			userPwd = Integer.parseInt(sc.nextLine());
-			
-			
-			if(kind.equals("1")) 
-				UserController.userLogin(userId, userPwd, "student");
-			else if(kind.equals("2"))
-				UserController.userLogin(userId, userPwd, "teacher");
-			else
-				FailView.errorMessage("사용자 유형 입력이 틀렸습니다.");
-			
-		}
-	}
-	
-	
 	public static void main(String[] args) {
-		userLogin();
+		MenuView.userLoginMenu();
 	}
 
 }
